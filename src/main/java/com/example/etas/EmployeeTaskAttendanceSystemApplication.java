6d3dad8eaea7e6564209884ecf3e7cd8a4ee6922
zain-vepcon <mohammed.zain@vepconsoftsystems.com>
2026-06-28 14:53:09 +0530

    Initial commit - Employee Task & Attendance Management System

---
+++ src/main/java/com/example/etas/EmployeeTaskAttendanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.etas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTaskAttendanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTaskAttendanceSystemApplication.class, args);
+	}
+
+}
